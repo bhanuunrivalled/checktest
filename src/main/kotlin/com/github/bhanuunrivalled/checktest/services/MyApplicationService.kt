07e0fe1f4bdf68722bf76fe048aaf217b6283b62
GitHub Action <action@github.com>
2021-07-02 11:04:37 +0000

    Template cleanup

--- src/main/kotlin/com/github/bhanuunrivalled/checktest/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/bhanuunrivalled/checktest/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.bhanuunrivalled.checktest.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.bhanuunrivalled.checktest.MyBundle
 
 class MyApplicationService {
 
